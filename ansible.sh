4c92de5f01b795e2a18c3433aafe82e6ca1216ca
Yura <shupik3@gmail.com>
2024-04-13 17:03:29 +0300

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,4 @@
+  GNU nano 7.2                                               ansible.sh                                                        
 #!/bin/bash
 sudo apt update
 sudo apt install -y python3 python3-pip
@@ -8,3 +9,11 @@ sudo apt update
 sudo apt install -y ansible
 
 ansible --version
+
+rm -rf  /etc/ansible/ansible.cfg
+mv /home/ubuntu/1/ansible.cfg /etc/ansible/
+cd /etc/ansible/ && mkdir playbook && mkdir host
+
+
+
+
